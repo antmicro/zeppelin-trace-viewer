6c3594a6d5faf824c659f9a102b02cd3cefff8d7
Eryk Szpotanski <eszpotanski@antmicro.com>
2025-08-06 18:58:58 +0200

    [#81573] plots: time-based-plot: Implement pointer reflected on all time based plots
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

---
+++ src/plots/time-based-plot.tsx
@@ -0,0 +1,51 @@
+import { JSX } from 'preact/jsx-runtime';
+import * as fc from 'd3fc';
+import { Selection } from 'd3-selection';
+import { Atom } from '@speedscope/lib/atom';
+
+import Plot, { PlotBaseProps } from './base-plot';
+
+
+const timestampHoveredAtom = new Atom<{x: number, yProc: number} | null>(null, 'timestampHovered');
+
+
+export default abstract class TimeBasedPlot<D, T extends PlotBaseProps<D> = PlotBaseProps<D>> extends Plot<D, T> {
+    protected override _createPointer(): ((s: Selection<d3.BaseType, any, any, any>) => void) | null {
+        /* eslint-disable-next-line @typescript-eslint/no-unsafe-return,@typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access */
+        return fc.pointer().on("point", ([coord]: {x: number, y: number}[]) => {
+            if (!coord) {
+                console.debug("Missing coordinates");
+                timestampHoveredAtom.set(null);
+                return;
+            }
+
+            // find the closes datapoint to the pointer
+            const x = this.xScale.invert(coord.x as d3.NumberValue);
+            const y = this.yScale.invert(coord.y as d3.NumberValue);
+            const [yBegin, yEnd] = this.yScale.domain();
+            timestampHoveredAtom.set({
+                x,
+                yProc: (y - yBegin) / (yEnd - yBegin),
+            });
+        });
+    }
+
+    override render(): JSX.Element {
+        timestampHoveredAtom.subscribe(() => {
+            this.annotations.pop();
+            const timestamp = timestampHoveredAtom.get();
+            if (!timestamp) {
+                this.redraw();
+                return;
+            }
+
+            const {x, yProc} = timestamp;
+            const [yBegin, yEnd] = this.yScale.domain();
+            const d = this._findClosestPoint(x, yBegin + (yEnd - yBegin) * yProc);
+
+            this._addAnnotation(d);
+        });
+        return super.render();
+    }
+}
+
