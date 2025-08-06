edeef7f5a971b6031be802aa523f2e182bf35699
Eryk Szpotanski <eszpotanski@antmicro.com>
2025-08-06 17:36:29 +0200

    [#81289] top-bar: Implement "buttons category" that groups buttons and displays them in dropdown list
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

--- src/top-bar.tsx
+++ src/top-bar.tsx
@@ -2,43 +2,74 @@
  * The module defining top bar of the application.
  */
 
-import { JSX } from "preact";
-import { useTheme } from "@speedscope/views/themes/theme";
+import { JSX, VNode } from "preact";
+import { useEffect, useRef, useState } from "preact/hooks";
+import { memo } from "preact/compat";
 
 import style from '@styles/app.module.scss';
 import { useAtom } from "@speedscope/lib/atom";
 import { appRefAtom, profileGroupAtom } from "@speedscope/app-state";
-import { ButtonHTMLAttributes } from "preact/compat";
+import ClickAwayListener from "react-click-away-listener";
+
+import ImportIcon from "./icons/import";
+import ExportIcon from "./icons/export";
 
 
-/** The Speedscope like button template */
-function Button(props: Omit<ButtonHTMLAttributes, "style" | "onMouseEnter" | "onMouseLeave">) {
-    const theme = useTheme();
-
-    const buttonStyle: JSX.CSSProperties = {
-        backgroundColor: theme.selectionPrimaryColor,
-    };
-    return (
-        <button
-            style={buttonStyle}
-            onMouseEnter={(e) => e.target instanceof HTMLButtonElement && (e.target.style.backgroundColor = theme.selectionSecondaryColor)}
-            onMouseLeave={(e) => e.target instanceof HTMLButtonElement && (e.target.style.backgroundColor = theme.selectionPrimaryColor)}
-            {...props}
-        >{props.children}</button>
-    );
+interface ButtonsContainerProps {
+    /** The name of the buttons category, displayed at the top */
+    name: string
+    /** The buttons displayed in the dropdown section */
+    children: (VNode<HTMLButtonElement> | null)[]
 }
 
+/** The container for button that displays their category and on hover opens dropdown list with buttons */
+const ButtonsContainer = memo(({name, children}: ButtonsContainerProps) => {
+    const ref = useRef<HTMLDivElement>(null);
+    const dropdownRef = useRef<HTMLDivElement>(null);
+
+    const hideDropdown = () => ref.current?.classList.remove(style.clicked);
+
+    useEffect(() => {
+        for (const ch of dropdownRef.current?.childNodes ?? []) {
+            if (!(ch instanceof HTMLButtonElement)) {continue;}
+            ch.addEventListener('click', hideDropdown);
+        }
+    });
+    return (
+        <ClickAwayListener onClickAway={hideDropdown}>
+            <div ref={ref} className={style.category}>
+                <button
+                    onClick={() => ref.current?.classList.add(style.clicked)}
+                >{name}</button>
+                {/* Fills gap between category and dropdown, to make sure the hover is not interrupted */}
+                <div className={style['gap-filler']} />
+                <div ref={dropdownRef} className={style.dropdown}>
+                    {children}
+                </div>
+            </div>
+        </ClickAwayListener>
+    );
+});
+
 /** The top bar of the application */
-export default function TopBar(): JSX.Element {
+export default  memo((): JSX.Element => {
     const appRefSt = useAtom(appRefAtom);
-    const profileGroup = useAtom(profileGroupAtom);
+    const [traceLoadedSt, setTraceLoadedSt] = useState<boolean>(false);
+
+    profileGroupAtom.subscribe(() => {
+        setTraceLoadedSt((profileGroupAtom.get()?.profiles.length ?? 0) > 0);
+    });
 
     const buttons = (
-        <div className={style['button-container']}>
-            <Button
-                onClick={appRefSt?.current?.browseForFile}
-            >IMPORT</Button>
-            {profileGroup && <Button onClick={appRefSt?.current?.saveFile}>EXPORT</Button>}
+        <div className={style['category-container']}>
+            <ButtonsContainer name="File">
+                <button onClick={appRefSt?.current?.browseForFile}>
+                    <ImportIcon /><p>Import trace</p>
+                </button>
+                {traceLoadedSt ? <button onClick={appRefSt?.current?.saveFile}>
+                    <ExportIcon /><p>Export trace</p>
+                </button> : null}
+            </ButtonsContainer>
         </div>
     );
 
@@ -48,4 +79,4 @@ export default function TopBar(): JSX.Element {
             {appRefSt?.current && buttons}
         </div>
     );
-}
+});
