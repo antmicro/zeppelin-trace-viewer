6f2b47675aa7421cbf27fa37e0e8f0a412460616
Eryk Szpotanski <eszpotanski@antmicro.com>
2025-08-20 18:37:15 +0200

    [#82041] top-bar: tiling-component-button: Implement button representing tiling component
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

---
+++ src/top-bar/tiling-component-button.tsx
@@ -0,0 +1,57 @@
+/**
+ * The module defining custom button used to spawn new tiling panels.
+ */
+
+import { memo } from "preact/compat";
+import { useAtom } from "@speedscope/lib/atom";
+
+import style from "@styles/app.module.scss";
+import { TilingComponent } from "@/utils/tiling-component";
+import { TilingLayoutProps } from "@/tiling-layout";
+
+
+/** Stores information about currently dragged button (represented by panel title) */
+let DRAGGED_BUTTON: string | undefined = undefined;
+
+/** Returns currently dragged button */
+export function getDraggedButtonTitle() {
+    return DRAGGED_BUTTON;
+}
+
+interface TilingComponentButtonProps extends Pick<TilingLayoutProps, "tilingRef"> {
+    /** The component definition */
+    component: TilingComponent<any>,
+}
+
+/** The button representing tiling component, displayed only when it is available and disabled if max instance count is reached */
+export const TilingComponentButton = memo(({component, tilingRef}: TilingComponentButtonProps) => {
+    const availableSt = useAtom(component.available);
+    const instancesSt = useAtom(component.instances);
+
+    const onClick = (_e: Event) => {
+        if (!tilingRef?.current) {
+            console.warn(`New panel (${component.title}) cannot be added, due to missing reference`);
+            return;
+        }
+        tilingRef.current.addNode(component);
+    };
+    const maxInstanceCountReached = instancesSt >= component.maxInstances;
+    return (
+        <button
+            className={style["panel-button"]}
+            style={availableSt ? {} : {display: "none"}}
+            disabled={maxInstanceCountReached}
+            onClick={onClick}
+        >
+            {/* Draggable div (instead of simple text) required to make the whole button grabbable */}
+            <div
+                onDragStart={() => DRAGGED_BUTTON = component.title}
+                onDragEnd={() => DRAGGED_BUTTON = undefined}
+                draggable={!maxInstanceCountReached}
+            >
+                {component.title}
+            </div>
+        </button>
+    );
+});
+
