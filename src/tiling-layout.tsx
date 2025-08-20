e3b7b1419a1888371236db5024232fc891744086
Eryk Szpotanski <eszpotanski@antmicro.com>
2025-08-20 18:39:59 +0200

    [#82041] tiling-layout: Add external drag&drop support and support counting instances of tiling components
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

--- src/tiling-layout.tsx
+++ src/tiling-layout.tsx
@@ -2,19 +2,34 @@
  * The module with tiling manager layout.
  */
 
-import { ComponentChild } from "preact";
-import { memo } from "preact/compat";
-import { Actions, DockLocation, IJsonModel, Layout, Model, Node, Orientation, TabNode } from "flexlayout-react";
+import { ComponentChild, RefObject } from "preact";
+import { memo, useImperativeHandle } from "preact/compat";
+import { Action, Actions, DockLocation, IJsonModel, Layout, Model, Node, Orientation, TabNode } from "flexlayout-react";
 import { metadataAtom } from "@speedscope/app-state";
 
 import style from "@styles/app.module.scss";
 import Speedscope from "./speedscope";
-import InfoPanel from './info-panel';
-import MemoryPanel from "./info-panel/memory-panel";
-import CPULoadPanel from "./info-panel/cpu-load-panel";
-import DieTempPanel from './info-panel/die-temp-panel';
-import { TilingComponent, createJSONNode, getTilingComponent } from "./utils/tiling-component";
+// Import all panels to make sure they are registered
+import * as panels from "./info-panel";
+import { TilingComponent, getAllComponents, getTilingComponent } from "./utils/tiling-component";
+import { getDraggedButtonTitle } from "./top-bar/tiling-component-button";
 
+
+const InfoPanel = panels.InfoPanel;
+
+/** The functions managing tiling layout state available from outside the component */
+export interface TilingRef {
+    /** Adds new panel with a given component */
+    addNode: (type_: TilingComponent<any>) => void,
+    /** Removes all panel with a given type */
+    removeNode: (type_: TilingComponent<any>) => void,
+}
+
+
+export interface TilingLayoutProps {
+    /** The reference that will contain tiling layout methods */
+    tilingRef: RefObject<TilingRef>,
+}
 
 /** The schema of initial layout */
 const initLayout: IJsonModel = {
@@ -30,17 +45,19 @@ const initLayout: IJsonModel = {
                 children: [
                     {
                         type: "tabset",
-                        children: [createJSONNode(Speedscope)],
+                        children: [Speedscope.createJSONNode()],
                     },
                     {
                         type: "tabset",
-                        children: [createJSONNode(InfoPanel)],
+                        children: [InfoPanel.createJSONNode()],
                     },
                 ],
             },
         ],
     },
 };
+// Increase instance quantity for initial components
+[Speedscope, InfoPanel].forEach(component => component.instances.set(1));
 
 
 /**
@@ -56,47 +73,67 @@ const MAX_NODES_IN_ROW_FOR_SPAWNING = 3;
  * It has to be stateless, otherwise Speedscope reloads (when the state is updated)
  * which leads to circular dependencies and infinite reloads.
  */
-export default memo(() => {
+export default memo(({tilingRef}: TilingLayoutProps) => {
+    useImperativeHandle(tilingRef, () => {
+        return {
+            addNode,
+            removeNode,
+        };
+    }, []);
+
     const model = Model.fromJson(initLayout);
-    // Retrieve node with InfoPanel
-    let infoPanelTab: Node;
-    model.visitNodes((n, _l) => {(n.getAttr("name") === InfoPanel.title) && (infoPanelTab = n);});
+    // Retrieve node with InfoPanel and speedscope
+    let infoPanelTab: TabNode;
+    let speedscopeTab: TabNode;
+    model.visitNodes((n, _l) => {
+        if (!(n instanceof TabNode)) {return;}
+        const comp = n.getAttr("component") as string | undefined;
+        if (comp === InfoPanel.title) {(infoPanelTab = n);}
+        else if (comp === Speedscope.title) {(speedscopeTab = n);}
+    });
 
     /**
      * Adds node to the parent of InfoPanel.
      * If there is no data for new node, it will not be created.
      */
     function addNode<T>(type_: TilingComponent<T>) {
-        // Prepare data for the new node
-        const data = type_.dataProvider ? type_.dataProvider() : undefined;
-        if (type_.dataProvider && !data) {return;}
-
-        let infoParent: Node | undefined = infoPanelTab;
+        // Choose info panel (if it's present on the same window as Speedscope) or fallback to speedscope
+        let parentNode: Node | undefined = (
+            model.getNodeById(infoPanelTab.getId()) && speedscopeTab.getWindowId() === infoPanelTab.getWindowId()
+        ) ? infoPanelTab : speedscopeTab;
         while (
-            infoParent &&
+            parentNode &&
             // Get next parent as long as current node is tab or is not horizontal
-            (infoParent.getType() === "tab" || infoParent?.getOrientation() !== Orientation.HORZ)
-        ) { infoParent = infoParent?.getParent();}
-        if (model.getRoot().getId() === infoParent?.getId()) { infoParent = infoPanelTab.getParent(); }
-        if (!infoParent) {
+            (parentNode.getType() === "tab" || parentNode?.getOrientation() !== Orientation.HORZ)
+        ) { parentNode = parentNode?.getParent();}
+        if (model.getRoot().getId() === parentNode?.getId()) { parentNode = infoPanelTab.getParent(); }
+        if (!parentNode) {
             console.warn("Parent of info panel is missing - new nodes will not be added");
             return;
         }
 
-        const children = infoParent.getChildren();
+        const children = parentNode.getChildren();
         const tooManyNodes = (children.length ?? 0) >= MAX_NODES_IN_ROW_FOR_SPAWNING;
-        model.doAction(Actions.addNode(
-            createJSONNode(type_, data),
-            (tooManyNodes ? children[children.length - 1].getId() : infoParent.getId()),
+        // doAction does not trigger onAction callback, therefore instance count has to be increased manually
+        type_.incrInstances();
+        const addedNode = model.doAction(Actions.addNode(
+            type_.createJSONNode(),
+            (tooManyNodes ? children[children.length - 1].getId() : parentNode.getId()),
             (tooManyNodes ? DockLocation.CENTER : DockLocation.RIGHT),
             -1,
             true,
-        ));
+        )) as (Node | undefined);
+
+        if (!addedNode) {
+            console.error(`Node ${type_.title} has not been added`);
+        }
     };
-    /** Removes the node with given ID */
+    /** Removes all panel with the given type */
     function removeNode<T>(type_: TilingComponent<T>) {
         model.visitNodes(n => {
-            if (n.getAttr("name") === type_.title) {
+            if (n.getAttr("component") === type_.title) {
+                // doAction does not trigger onAction callback, therefore instance count has to be decreased manually
+                type_.decrInstances();
                 model.doAction(Actions.deleteTab(n.getId()));
             }
         });
@@ -109,7 +146,8 @@ export default memo(() => {
             console.warn("Factory: component not defined");
             return;
         }
-        const ComponentType = getTilingComponent(component);
+        const tilingComponent = getTilingComponent(component);
+        const ComponentType = tilingComponent?.component;
         if (!ComponentType) {
             console.warn(`Factory: component '${component}' not registered`);
             return;
@@ -118,12 +156,42 @@ export default memo(() => {
         return <ComponentType {...(node.getConfig() ?? {})} />;
     };
 
+    // Actions when new trace is loaded
     metadataAtom.subscribe(() => {
-        // Delete outdated panels
-        [MemoryPanel, CPULoadPanel, DieTempPanel].forEach(removeNode);
-        // On each metadata update, try to create new panels
-        [MemoryPanel, CPULoadPanel, DieTempPanel].forEach(addNode);
+        // Delete all panels apart from Speedscope and info panel
+        getAllComponents().filter((v) => ![Speedscope, InfoPanel].includes(v as TilingComponent<object>)).forEach(removeNode);
+
+        // On each metadata update, create available panels
+        [panels.MemoryPanel, panels.CPULoadPanel, panels.DieTempPanel].filter(v => v.available.get()).forEach(addNode);
+
     });
+
+    /** External drag callback, that spawns new panel if a button associated with a component is dragged */
+    const onExternalDrag = (_) => {
+        const draggedButton = getDraggedButtonTitle();
+        if (!draggedButton) {return undefined;}
+        const json = getTilingComponent(draggedButton)?.createJSONNode();
+        if (!json) {return undefined;}
+        return {json};
+    };
+
+    /** On action callback that increases and decreases number of components instances */
+    const onAction = (a: Action) => {
+        if (a.type === Actions.ADD_NODE && "component" in a.data.json) {
+            /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access */
+            const tilingComponent = getTilingComponent(a.data.json.component as string);
+            if (tilingComponent) {
+                tilingComponent.incrInstances();
+            }
+        } else if (a.type === Actions.DELETE_TAB) {
+            const n = model.getNodeById(a.data.node as string);
+            const tilingComponent = getTilingComponent(n?.getAttr("component") as string);
+            if (tilingComponent) {
+                tilingComponent.decrInstances();
+            }
+        }
+        return a;
+    };
 
     return (
         <div id={style['tiling-layout']}>
@@ -132,6 +200,8 @@ export default memo(() => {
                 factory={factory}
                 popoutWindowName="Zeppelin Trace Viewer"
                 realtimeResize={true}
+                onExternalDrag={onExternalDrag}
+                onAction={onAction}
             />
         </div>
     );
