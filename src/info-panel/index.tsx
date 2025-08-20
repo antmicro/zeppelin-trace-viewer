34f949ced5fa0d6b9141d3618a620514acbc3db4
Eryk Szpotanski <eszpotanski@antmicro.com>
2025-08-20 18:35:58 +0200

    [#82041] info-panel: Move info panel to dedicated file and import all panels in index file
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

--- src/info-panel/index.tsx
+++ src/info-panel/index.tsx
@@ -1,45 +1,10 @@
 /**
- * The module implementing additional information panel.
- * It, based on user activity and used profile, creates additional panels
- * like memory usage or model information.
+ * The module gathering all panel definitions.
  */
 
-import styles from "@styles/info-panel.module.scss";
-import PanelTemplate from './common';
-import ModelInfoPanel from './model-panel';
-import { useFrameProvider } from '@/utils/frame-provider';
-import { ModelEventArgs, ModelEventName, SpeedscopeFrameArgs } from '@/event-types';
-import tilingComponent from '@/utils/tiling-component';
+import InfoPanel from "./info-panel";
+import MemoryPanel from "./memory-panel";
+import CPULoadPanel from "./cpu-load-panel";
+import DieTempPanel from "./die-temp-panel";
 
-
-interface GenericInfoProps {
-    /** The information displayed by GenericInfo component */
-    info: string,
-}
-
-/** The basic information panel with simple text */
-function GenericInfo({info}: GenericInfoProps) {
-    return (
-        <PanelTemplate>
-            <p className={styles["generic-info"]}>{info}</p>
-        </PanelTemplate>
-    );
-}
-
-
-/**
- * The info panel managing all subpanels based on loaded profile or selected event.
- */
-function InfoPanel() {
-    const { frameSt } = useFrameProvider();
-
-    if (typeof(frameSt?.key) === "string" && frameSt?.key?.startsWith(`${ModelEventName}::`)) {
-        return <ModelInfoPanel frameArgs={frameSt.args as SpeedscopeFrameArgs<ModelEventArgs>} />;
-    } else if (frameSt) {
-        return <GenericInfo info="The chosen event does not contain additional information" />;
-    }
-
-    return <GenericInfo info="For more info, chose an event" />;
-}
-
-export default tilingComponent(InfoPanel, "Additional Info", {enablePopout: true})!;
+export {InfoPanel, MemoryPanel, CPULoadPanel, DieTempPanel};
